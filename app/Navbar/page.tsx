e43fbe8ef314424335343f29975c4ebfd321836a
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-18 19:29:37 +0100

    __

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
 
-type Props = {}
+type Props = {
+}
 
 const page = (props: Props) => {
   return (
-    <div className='flex w-[1340px] h-[119px] justify-around bg-[--navColor] ml-[100px] mr-[100px]'>
-        <div className=""><img src="logo.png" alt="" /></div>
-        <div className="">.</div>
-        <div className="">.</div>
+    <div className='flex w-[1340px] h-[119px] justify-between bg-[--navColor] ml-[100px] mr-[100px] mt-[9px] items-center'>
+        <div className="">
+            <Image width={391} height={44} src="/logo.png" alt=''/>
+        </div>
+        {/* <Image src="logo.png" alt=''/> */}
+        <div className="flex">
+            <ul className='flex uppercase'>
+                <li>Forside</li>
+                <li>Services/Prise</li>
+                <li>portfolio</li>
+                <li>galeri</li>
+                <li>Om os</li>
+                <li>Kontakt</li>
+            </ul>
+        </div>
+        <div className="">
+        <Image width={32} height={41} src="logo.png" alt=''/>
+        <Image width={32} height={41} src="logo.png" alt=''/>
+        </div>
     </div>
   )
 }
