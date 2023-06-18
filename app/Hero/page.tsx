1a0b1844c6a36d1a6e45dde459b9d42bb2226765
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-18 21:53:25 +0100

    texts

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -6,9 +6,9 @@ type Props = {}
 const page = (props: Props) => {
   return (
     <div className='flex'>
-        <div className="">
-            <h1>THE ULTIMATE MYSTERY FOOTBALL SHIRT EXPERIENCE.</h1>
-            <p className="">Order a one-time box for the footy fanatic in your life or join the ever-growing club on a mystery football shirt subscription!</p>
+        <div className="ml-[173px] mt-[52px]">
+            <h1 className='text-[40px] font-extrabold'>THE ULTIMATE MYSTERY FOOTBALL SHIRT EXPERIENCE.</h1>
+            <p className="text-[32] font-normal leading-5">Order a one-time box for the footy fanatic in your life or join the ever-growing club on a mystery football shirt subscription!</p>
         </div>
         <div className="mr-[42px]">
              <Image width={672} height={378} src="/home-hero.png" alt=''/ >
