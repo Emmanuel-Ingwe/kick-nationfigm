900962c4c1cb031d4c86449a335c0587c9e755b5
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-18 16:20:38 +0100

    config

--- app/Searchbar/page.tsx
+++ app/Searchbar/page.tsx
@@ -4,8 +4,13 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>
-        <input type="text" placeholder='Search...'/>
+    <div className='w-screen'>
+        <div className="absolute left-[1103px] top-[19px]">
+        <input className='w-[395px] h-[47.43px] border border-[--color-blue] rounded-3xl pl-6 placeholder-black outline-none' type="text" placeholder='Search...' />
+        </div>
+        <div className='absolute top-[86px] left-[102px]'>
+            <h2>Fri Fragt på ordre over 600 DKK Dansk Lager hurtig levering Lager Indregistrering autentiske trøjer </h2>
+        </div>
     </div>
   )
 }
