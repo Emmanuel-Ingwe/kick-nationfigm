d39641f7db56d58d4ab3e09391f8037e736d5d8d
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-18 21:30:05 +0100

    hero

---
+++ app/Hero/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className='flex'>
+        <div className="">.</div>
+        <div className="">.</div>
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Search from "./Searchbar/page";
 import Nav from "./Navbar/page"
+import Hero from "./Hero/page"
 
 export default function Home() {
   return (
     <main className="">
       <Search />
       <Nav />
+      <Hero />
     </main>
   )
 }
