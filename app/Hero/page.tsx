c0c67c55db6f605c61914589662b310f7129d0fd
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-18 21:35:51 +0100

    hero img

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div className='flex'>
-        <div className="">.</div>
-        <div className="">.</div>
+        <div className="">
+            <h2>THE ULTIMATE MYSTERY FOOTBALL SHIRT EXPERIENCE.</h2>
+            <p className="">Order a one-time box for the footy fanatic in your life or join the ever-growing club on a mystery football shirt subscription!</p>
+        </div>
+        <div className="">
+             <Image width={32} height={41} src="/tshit.png" alt=''/ >
+        </div>
     </div>
   )
 }
