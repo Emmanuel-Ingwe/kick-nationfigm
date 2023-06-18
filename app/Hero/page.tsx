ac533b1b2f1cfe0b800a8f23fd274e48d3dc29ba
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-18 21:42:09 +0100

    _

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -10,7 +10,7 @@ const page = (props: Props) => {
             <h1>THE ULTIMATE MYSTERY FOOTBALL SHIRT EXPERIENCE.</h1>
             <p className="">Order a one-time box for the footy fanatic in your life or join the ever-growing club on a mystery football shirt subscription!</p>
         </div>
-        <div className="">
+        <div className="mr-[42px]">
              <Image width={672} height={378} src="/home-hero.png" alt=''/ >
         </div>
     </div>
