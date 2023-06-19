7f44b87960fae5266a6e69375140b81349df7167
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-19 17:34:23 +0100

    done w hero

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -6,11 +6,11 @@ type Props = {}
 const page = (props: Props) => {
   return (
     <div className='flex'>
-        <div className="ml-[173px] mt-[52px]">
+        <div className="ml-[173px] mt-[40px]">
             <div className="flex flex-wrap">
-                <h1 className='text-[37px] font-extrabold w-[672px]'>THE ULTIMATE MYSTERY FOOTBALL SHIRT EXPERIENCE.</h1>
+                <h1 className='text-[37px] font-extrabold w-[572px]'>THE ULTIMATE <br /> MYSTERY FOOTBALL SHIRT EXPERIENCE.</h1>
             </div>
-            <div className="flex flex-wrap mt-3">
+            <div className="flex flex-wrap mt-2.5">
                 <p className="text-[20px] font-normal leading-7 w-1/2">Order a one-time box for the footy fanatic in your life or join the ever-growing club on a mystery football shirt subscription!</p>
             </div>
         </div>
