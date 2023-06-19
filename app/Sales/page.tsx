53565b5b25ad4b35db6fa9235ee03fa39188874a
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-20 00:12:18 +0100

    icons

--- app/Sales/page.tsx
+++ app/Sales/page.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 type item = {
+    id: number
     icon: string
     name: string
     description: string
 }
 
 const items: item[] = [
-    {icon: "tshirt.png", name: "Premium Fodboldtrøje", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
-    {icon: "boxes.png", name: "Mystery Boxes", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
-    {icon: "portrait.png", name: "Plakatar", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
+    {id:1, icon: "/tshit.png", name: "Premium Fodboldtrøje", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
+    {id:2, icon: "/boxes.png", name: "Mystery Boxes", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
+    {id:3, icon: "/portrait.png", name: "Plakatar", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
 ]
 
 const page = (props: Props) => {
   return (
     <div>
-        items.map((item: Item) {
-            
-        })
+        <div className="">
+            {items.map((item: item) => (
+                <Image key={item.id} width={32} height={41} src={item.icon} alt=''/ >
+            ))}
+        </div>
     </div>
   )
 }
