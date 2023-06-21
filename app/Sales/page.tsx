3e89ae6311dcda7d4a7d94d0f71c13e23abfe41e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-21 08:42:39 +0100

    __

--- app/Sales/page.tsx
+++ app/Sales/page.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 type Props = {}
 
-type item = {
+type Item = {
     id: number
     icon: string
     name: string
     description: string
 }
 
-const items: item[] = [
+const items: Item[] = [
     {id:1, icon: "/tshit.png", name: "Premium Fodboldtrøje", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
     {id:2, icon: "/boxes.png", name: "Mystery Boxes", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
     {id:3, icon: "/portrait.png", name: "Plakatar", description: "Shop i blandt vores udvalg af Premium fodboldtrøjer, her kan du finde yderst sjældne og eftertragtede fodboldtrøjer"},
@@ -19,11 +19,15 @@ const items: item[] = [
 const page = (props: Props) => {
   return (
     <div>
-        <div className="">
-            {items.map((item: item) => (
-                <Image key={item.id} width={32} height={41} src={item.icon} alt=''/ >
+            {items.map((item: Item) => (
+            <div key={item.id} className="">
+                <Image width={32} height={41} src={item.icon} alt=''/ >
+                 <div className="">
+                     <h3>{item.name}</h3>
+                     <p>{item.description}</p>
+                 </div>
+            </div>
             ))}
-        </div>
     </div>
   )
 }
