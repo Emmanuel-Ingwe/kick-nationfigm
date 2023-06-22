ffcfac9834fa38305e761b159cffc6bea824a16b
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-22 01:49:33 +0100

    done w foot

--- app/Features/page.tsx
+++ app/Features/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div className='container mt-[46px] flex flex-col'>
+    <div className='container mt-[46px] flex flex-col items-center mb-[89px]'>
       <h1 className="text-center font-extrabold text-[40px]">FEATURES PRODUCTS/ SHOP</h1>
+      <Image className='ml-[36px] cursor-progress' width={1265} height={485} src="/footer.png" alt=''/ >
     </div>
   )
 }
